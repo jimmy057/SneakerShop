f37c725d9646da65fcf26abe2b9cb257226d1f7d
jimmy057 <jimmypolanco2540@gmail.com>
2025-11-25 23:00:35 -0400

    Datastore creado

--- app/src/main/java/com/example/proyectofinal/data/local/seccion/SessionDataStore.kt
+++ app/src/main/java/com/example/proyectofinal/data/local/seccion/SessionDataStore.kt
@@ -5,36 +5,48 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Singleton
 
 private const val DATASTORE_NAME = "session_prefs"
+private val Context._dataStore by preferencesDataStore(name = DATASTORE_NAME)
 
-val Context.dataStore by preferencesDataStore(name = DATASTORE_NAME)
-
-class SessionDataStore(private val context: Context) {
-
+@Singleton
+class SessionDataStore @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
     private object Keys {
         val TOKEN = stringPreferencesKey("token")
         val NOMBRE = stringPreferencesKey("nombre")
     }
+
     suspend fun saveSession(token: String, nombre: String) {
-        context.dataStore.edit { prefs ->
+        context._dataStore.edit { prefs ->
             prefs[Keys.TOKEN] = token
             prefs[Keys.NOMBRE] = nombre
         }
     }
 
-    val tokenFlow = context.dataStore.data
-        .catch { exception ->
-            if (exception is IOException) emit(emptyPreferences()) else throw exception
+    suspend fun clearSession() {
+        context._dataStore.edit { prefs ->
+            prefs.remove(Keys.TOKEN)
+            prefs.remove(Keys.NOMBRE)
+        }
+    }
+
+    val tokenFlow = context._dataStore.data
+        .catch { e ->
+            if (e is IOException) emit(emptyPreferences()) else throw e
         }
         .map { prefs -> prefs[Keys.TOKEN] ?: "" }
 
-    val nombreFlow = context.dataStore.data
-        .catch { exception ->
-            if (exception is IOException) emit(emptyPreferences()) else throw exception
+    val nombreFlow = context._dataStore.data
+        .catch { e ->
+            if (e is IOException) emit(emptyPreferences()) else throw e
         }
         .map { prefs -> prefs[Keys.NOMBRE] ?: "" }
 }
