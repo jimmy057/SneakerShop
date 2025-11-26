56ceec30bc3a2650324c7a10fdb2f6a9b340fbe4
jimmy057 <jimmypolanco2540@gmail.com>
2025-11-25 23:02:25 -0400

    AppModule creado

--- app/src/main/java/com/example/proyectofinal/di/AppModule.kt
+++ app/src/main/java/com/example/proyectofinal/di/AppModule.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Room
 import com.example.proyectofinal.data.local.Dao.UserDao
 import com.example.proyectofinal.data.local.database.AppDatabase
-import com.example.proyectofinal.data.local.seccion.SessionDataStore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,16 +17,10 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideDatabase(@ApplicationContext ctx: Context): AppDatabase {
-        return Room.databaseBuilder(ctx, AppDatabase::class.java, "app_db").build()
-    }
+    fun provideDatabase(@ApplicationContext ctx: Context): AppDatabase =
+        Room.databaseBuilder(ctx, AppDatabase::class.java, "app_db").build()
 
     @Provides
     fun provideUserDao(db: AppDatabase): UserDao = db.userDao()
-
-    @Provides
-    @Singleton
-    fun provideSessionDataStore(@ApplicationContext ctx: Context): SessionDataStore =
-        SessionDataStore(ctx)
 
 }
